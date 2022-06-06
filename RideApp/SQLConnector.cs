c38bfdde8917196f96594c9465f4a449a0f2fcb5
anirudh <anirudh8987@gmail.com>
2022-06-07 01:02:48 +0530

    Changes done to make "west" reference in android and ios projects

--- RideApp/SQLConnector.cs
+++ RideApp/SQLConnector.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
+//using Microsoft.Data.SqlClient;
 
 
 namespace RideApp
@@ -12,31 +13,63 @@ namespace RideApp
     {
         public static void InsertData(string phoneNo)
         {
+            //    try
+            //    {
+            //        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            //        builder.DataSource = "ridedbserver.database.windows.net";
+            //        builder.UserID = "ridemaster";
+            //        builder.Password = "Anir#0027";
+            //        builder.InitialCatalog = "Ride";
+            //        using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            //        {
+            //            String sql = "insert into [dbo].[userdetails](phoneno) values('9884391041')";
+
+            //            using (SqlCommand command = new SqlCommand(sql, connection))
+            //            {
+            //                connection.Open();
+            //                int rowsAffected = command.ExecuteNonQuery();
+            //                connection.Close();
+
+            //            }
+            //        }
+            //    }
+            //    catch (SqlException e)
+            //    {
+            //        Console.WriteLine(e.ToString());
+            //    }
+            //}
             try
             {
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "ridedbserver.database.windows.net";
-                builder.UserID = "ridemaster";
-                builder.Password = "Anir#0027";
-                builder.InitialCatalog = "Ride";
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                var builder = new SqlConnection("Server=tcp:ridedbserver.database.windows.net,1433;Initial Catalog=Ride;Persist Security Info=False;User ID=ridemaster;Password=Anir#0027;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                builder.Open();
+                //builder.ConnectionString = 
+                //    "Data Source=ridedbserver.database.windows.net;"+                    
+                //    "Initial Catalog=Ride;" +
+                //    "User id=ridemaster;" +
+                //    "Password=Anir#0027;";
+                // builder.DataSource = "ridedbserver.database.windows.net";
+                // builder.UserID = "ridemaster";
+                // builder.Password = "Anir#0027";
+                // builder.InitialCatalog = "Ride";                
+                //using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+
+                String sql = "insert into [dbo].[userdetails](phoneno) values('9884391041')";
+
+                using (SqlCommand command = new SqlCommand(sql, builder))
                 {
-                    String sql = "insert into [dbo].[userdetails](phoneno) values('9884391041')";
+                    //connection.Open();
+                    
+                    int rowsAffected = command.ExecuteNonQuery();
+                    builder.Close();
 
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        connection.Open();
-                        int rowsAffected = command.ExecuteNonQuery();
-                        connection.Close();
-
-                    }
                 }
+
             }
             catch (SqlException e)
             {
                 Console.WriteLine(e.ToString());
             }
         }
-    }
+    }    
 }
 
